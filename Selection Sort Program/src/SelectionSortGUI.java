a8c5cb47c5d99116a326bc6d32b464f1418509e6
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2020-02-10 08:28:40 -0500

    2/10

---
+++ Selection Sort Program/src/SelectionSortGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import BreezySwing.*;
+
+public class SelectionSortGUI  extends GBFrame {
+	
+	public static void main(String[] args) {
+		JFrame frm = new SelectionSortGUI();
+		frm.setTitle("Student Grades");
+		frm.setSize(300, 300);
+		frm.setVisible(true);
+		
+	}
+	
+	JButton newStudentButton = addButton("New Student",1,1,1,1);
+	JButton 
+	
+}
