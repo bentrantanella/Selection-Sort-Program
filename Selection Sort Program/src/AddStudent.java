b5056a40feccfab8eb8427ba2acdd1491e42a0e7
bentrantanella <bentrantanella@10.0.0.61>
2020-02-11 11:36:06 -0500

    2/11

--- Selection Sort Program/src/AddStudent.java
+++ Selection Sort Program/src/AddStudent.java
@@ -9,6 +9,13 @@ public class AddStudent extends GBDialog {
 		setSize(300, 300);
 	}
 	
-	JLabel nameLabel = addLabel("Name:",1,1,1,1);
-	JTextField nameField = addTextField("",1,2,1,1);
+	JLabel nameLabel = addLabel("Name:",1,1,2,1);
+	JTextField nameField = addTextField("",1,2,2,1);
+	
+	JLabel testLabel = addLabel("Test score:",2,1,1,1);
+	IntegerField testField = addIntegerField(0,2,2,1,1);
+	JButton addButton = addButton("Add",2,3,1,1);
+	JLabel testCountLabel = addLabel("Scores entered: 0",2,4,1,1);
+	
+	
 }
