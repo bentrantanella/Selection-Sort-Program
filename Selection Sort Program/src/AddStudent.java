d3669c2caad98ef4f5f969b41a4987c2afc0a1a9
20btrantanella <20btrantanella@WA-248-9579.schools.westford-ma.gov>
2020-02-12 10:19:25 -0500

    2/12

--- Selection Sort Program/src/AddStudent.java
+++ Selection Sort Program/src/AddStudent.java
@@ -17,13 +17,42 @@ public class AddStudent extends GBDialog {
 	JButton addTestButton = addButton("Add",2,3,1,1);
 	JLabel testCountLabel = addLabel("Scores entered: 0",2,4,1,1);
 	
-	JLabel quizLabel = addLabel("Quiz score:",2,1,1,1);
-	IntegerField quizField = addIntegerField(0,2,2,1,1);
-	JButton addQuizButton = addButton("Add",2,3,1,1);
-	JLabel quizCountLabel = addLabel("Scores entered: 0",2,4,1,1);
+	JLabel quizLabel = addLabel("Quiz score:",3,1,1,1);
+	IntegerField quizField = addIntegerField(0,3,2,1,1);
+	JButton addQuizButton = addButton("Add",3,3,1,1);
+	JLabel quizCountLabel = addLabel("Scores entered: 0",3,4,1,1);
 	
-	JLabel hwLabel = addLabel("Homework Average:",2,1,1,1);
-	IntegerField hwField = addIntegerField(0,2,2,1,1);
-	JButton addButton = addButton("Add",2,3,1,1);
+	JLabel hwLabel = addLabel("Homework Average:",4,1,1,1);
+	IntegerField hwField = addIntegerField(0,4,2,1,1);
+	
+	JButton addStudentButton = addButton("Add Student",5,1,2,1);
+	
+	int testcount = 0;
+	int quizcount = 0;
+	int[] tests = new int[5];
+	
+	public void buttonClicked(JButton button) {
+		if (button == addTestButton) {
+			if (testField.getNumber() < 0 || !testField.isValidNumber()) {
+				messageBox("Invalid input");
+				return;
+			}
+			tests[testcount] = testField.getNumber();
+			testcount++;
+			testField.setNumber(0);
+			testCountLabel.setText("Scores entered: 1");
+		}
+		
+		if (button == addQuizButton) {
+			if (quizField.getNumber() < 0 || !quizField.isValidNumber()) {
+				messageBox("Invalid Input");
+				return;
+			}
+			
+			
+		}
+		
+		
+	}
 	
 }
